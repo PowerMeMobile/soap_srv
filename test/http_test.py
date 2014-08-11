a89dba5a5a16100a985c1a73c4ec2d6e868a61fd
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-08-11 16:55:41 +0300

    s/make_(.*)_req/\1/

--- test/http_test.py
+++ test/http_test.py
@@ -57,31 +57,31 @@ def request(request):
 # Utils
 #
 
-def make_authenticate_req(request, customerID, userName, userPassword):
+def authenticate(request, customerID, userName, userPassword):
     url = HOST + '/HTTP_Authenticate'
     params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword}
     return request.make(url, params)
 
-def make_keep_alive_req(request, customerID, userName, userPassword):
+def keep_alive(request, customerID, userName, userPassword):
     url = HOST + '/HTTP_KeepAlive'
     params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword}
     return request.make(url, params)
 
-def make_send_sms_req(request, customerID, userName, userPassword, originator, smsText, recipientPhone, messageType, defDate, blink, flash, Private):
+def send_sms(request, customerID, userName, userPassword, originator, smsText, recipientPhone, messageType, defDate, blink, flash, Private):
     url = HOST + '/HTTP_SendSms'
     params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword, \
     'originator': originator, 'smsText': smsText, 'recipientPhone': recipientPhone, 'messageType': messageType, \
     'defDate': defDate, 'blink': str(blink), 'flash': str(flash), 'Private': str(Private)}
     return request.make(url, params)
 
-def make_send_binary_sms_req(request, customerID, userName, userPassword, originator, binaryBody, recipientPhone, defDate, data_coding, esm_class, PID):
+def send_binary_sms(request, customerID, userName, userPassword, originator, binaryBody, recipientPhone, defDate, data_coding, esm_class, PID):
     url = HOST + '/HTTP_SendBinarySms'
     params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword, \
     'originator': originator, 'binaryBody': binaryBody, 'recipientPhone': recipientPhone, 'defDate': defDate, \
     'data_coding': data_coding, 'esm_class': esm_class, 'PID': PID}
     return request.make(url, params)
 
-def make_get_sms_status_req(request, customerID, userName, userPassword, transactionID, detailed):
+def get_sms_status(request, customerID, userName, userPassword, transactionID, detailed):
     url = HOST + '/HTTP_GetSmsStatus'
     params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword, \
     'transactionID': transactionID, 'detailed': str(detailed)}
@@ -92,7 +92,7 @@ def make_get_sms_status_req(request, customerID, userName, userPassword, transac
 #
 
 def test_Authenticate_bad_user_fail(request):
-    res = make_authenticate_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD)
+    res = authenticate(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD)
     assert res['AuthResult']['Result'] == '404.2 FAILURE (User is unknown)'
     assert res['AuthResult']['Originators'] == None
     assert res['AuthResult']['NetPoints'] == '0'
@@ -100,7 +100,7 @@ def test_Authenticate_bad_user_fail(request):
     assert res['AuthResult']['CustomerID'] == str(-1)
 
 def test_Authenticate_succ(request):
-    res = make_authenticate_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD)
+    res = authenticate(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD)
     assert res['AuthResult']['Result'] == 'OK'
     assert res['AuthResult']['Originators'] != []
     assert res['AuthResult']['NetPoints'] == 'POSTPAID'
@@ -112,11 +112,11 @@ def test_Authenticate_succ(request):
 #
 
 def test_KeepAlive_bad_user_fail(request):
-    res = make_keep_alive_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD)
+    res = keep_alive(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD)
     assert res['CommonResult']['Result'] == '404.2 FAILURE (User is unknown)'
 
 def test_KeepAlive_succ(request):
-    res = make_keep_alive_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD)
+    res = keep_alive(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD)
     assert res['CommonResult']['Result'] == 'OK'
 
 #
@@ -126,49 +126,49 @@ def test_KeepAlive_succ(request):
 # messageType=Latin|ArabicWithArabicNumbers|ArabicWithLatinNumbers
 
 def test_HTTP_SendSms_bad_user_fail(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == '404.2 FAILURE (User is unknown)'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendSms_bad_originator_fail(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator='', smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator='', smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == '600.1 Originator for customerID is not found'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendSms_no_recipient_fail(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone='', messageType='Latin', defDate='', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone='', messageType='Latin', defDate='', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == '600.4 Phone not specified'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendSms_bad_recipient_fail(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=BAD_RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=BAD_RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == 'FAILURE: All recipient numbers in your message are either Rejected or Blacklisted'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendSms_empty_body_fail(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == 'Message Content Is Empty'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendSms_bad_defdate_fail(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='2014-07-21', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='2014-07-21', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == 'Def Date format is incorrect. Correct format is YYYYMMDDHHMMSS'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendSms_succ(request):
-    res = make_send_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
+    res = send_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, smsText='Hello', recipientPhone=RECIPIENT, messageType='Latin', defDate='', blink=False, flash=False, Private=False)
     assert res['SendResult']['Result'] == 'OK'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == 'POSTPAID'
@@ -179,49 +179,49 @@ def test_HTTP_SendSms_succ(request):
 #
 
 def test_HTTP_SendBinarySms_bad_user_fail(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == '404.2 FAILURE (User is unknown)'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendBinarySms_bad_originator_fail(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator='', binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator='', binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == '600.1 Originator for customerID is not found'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendBinarySms_no_recipient_fail(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone='', defDate='', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone='', defDate='', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == '600.4 Phone not specified'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendBinarySms_bad_recipient_fail(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=BAD_RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=BAD_RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == 'FAILURE: All recipient numbers in your message are either Rejected or Blacklisted'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendBinarySms_empty_body_fail(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == 'Message Content Is Empty'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendBinarySms_bad_defdate_fail(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='2014-07-21', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='2014-07-21', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == 'Def Date format is incorrect. Correct format is YYYYMMDDHHMMSS'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == '0'
     assert res['SendResult']['TransactionID'] == None
 
 def test_HTTP_SendBinarySms_succ(request):
-    res = make_send_binary_sms_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
+    res = send_binary_sms(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, originator=ORIGINATOR, binaryBody='7465737420746573742074657374207465737420', recipientPhone=RECIPIENT, defDate='', data_coding='4', esm_class='', PID='')
     assert res['SendResult']['Result'] == 'OK'
     assert res['SendResult']['RejectedNumbers'] == None
     assert res['SendResult']['NetPoints'] == 'POSTPAID'
@@ -232,28 +232,28 @@ def test_HTTP_SendBinarySms_succ(request):
 #
 
 def test_HTTP_GetSmsStatus_bad_user_fail(request):
-    res = make_get_sms_status_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD, transactionID=TRANSACTION_ID, detailed=False)
+    res = get_sms_status(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=BAD_PASSWORD, transactionID=TRANSACTION_ID, detailed=False)
     assert res['SmsStatus']['Result'] == '404.2 FAILURE (User is unknown)'
     assert res['SmsStatus']['NetPoints'] == '0'
 
 def test_HTTP_GetSmsStatus_empty_transaction_id_fail(request):
-    res = make_get_sms_status_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID='', detailed=False)
+    res = get_sms_status(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID='', detailed=False)
     assert res['SmsStatus']['Result'] == "605.7 The action you requested cannot be performed, because one of your the required request parameters ('TransactionID') was not supplied."
     assert res['SmsStatus']['NetPoints'] == '0'
 
 def test_HTTP_GetSmsStatus_wrong_transaction_id_fail(request):
-    res = make_get_sms_status_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID=BAD_TRANSACTION_ID, detailed=False)
+    res = get_sms_status(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID=BAD_TRANSACTION_ID, detailed=False)
     assert res['SmsStatus']['Result'] == 'SMS ID for status request is incorrect or not specified'
     assert res['SmsStatus']['NetPoints'] == '0'
 
 def test_HTTP_GetSmsStatus_detailed_false_succ(request):
-    res = make_get_sms_status_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID=TRANSACTION_ID, detailed=False)
+    res = get_sms_status(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID=TRANSACTION_ID, detailed=False)
     assert res['SmsStatus']['Result'] == 'OK'
     assert res['SmsStatus']['NetPoints'] == 'POSTPAID'
     assert res['SmsStatus']['Statistics']['statistics']['SMSC_DELIVERED']['#text'] == "1"
 
 def test_HTTP_GetSmsStatus_detailed_true_succ(request):
-    res = make_get_sms_status_req(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID=TRANSACTION_ID, detailed=True)
+    res = get_sms_status(request, customerID=CUSTOMER_ID, userName=USER_ID, userPassword=PASSWORD, transactionID=TRANSACTION_ID, detailed=True)
     assert res['SmsStatus']['Result'] == 'OK'
     assert res['SmsStatus']['NetPoints'] == 'POSTPAID'
     assert res['SmsStatus']['Statistics']['statistics']['SMSC_DELIVERED']['#text'] == "1"
