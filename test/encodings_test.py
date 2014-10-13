5929058272b2e2ee212bb470de9e7464c8a323cb
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-10-13 18:58:27 +0300

    Add utf8 messge parts tests

--- test/encodings_test.py
+++ test/encodings_test.py
@@ -14,79 +14,106 @@
 # $ python runtests.py -v
 
 import pytest
+import requests
+import xmltodict
 import hexdump
 
-SOAP11 = 'soapenv'
-
-WSDL = 'http://localhost:8088/bmsgw/soap/messenger.asmx?WSDL'
-#WSDL = 'http://mm.powermemobile.com/mm/soap/messenger.asmx?WSDL'
+HOST = 'http://localhost:8088/bmsgw/soap/messenger.asmx'
+#HOST = 'http://mm.powermemobile.com/mm/soap/messenger.asmx'
 
 CUSTOMER_ID = 3
 USER_ID     = 'user'
 PASSWORD    = 'password'
-BAD_PASSWORD = 'intentionally wrong password'
-
-USER = {
-    'CustomerID':CUSTOMER_ID,
-    'Name':USER_ID,
-    'Language':'en',
-    'Password':PASSWORD
-}
 
 ORIGINATOR = 'SMS'
-RECIPIENT = '375293615363'
-
-@pytest.fixture(scope="module")
-def client(request):
-    from pysimplesoap.client import SoapClient
-
-    wsdl = WSDL
-    soap_ns = SOAP11
-
-    client = SoapClient(wsdl=wsdl, soap_ns=soap_ns, trace=False)
-
-    # Statistics and Details parsers
-    # workaround of <s:sequence><s:any/></s:sequence>
-
-    # SMSC_DELIVERED, 3e231a28-cdf2-4d95-ac2e-a8cd1e0a0a0a - TempFail_WillRetry
-    statistics = {'SMSC_PENDING':int, 'SMSC_SUBMITTED':int, 'SMSC_FAILED':int, 'SMSC_DELIVERED':int, 'SMSC_EXPIRED':int, 'SMSC_DELETED':int, 'SMSC_UNDELIVERED':int, 'SMSC_ACCEPTED':int, 'SMSC_UNKNOWN':int, 'SMS_REJECTED':int}
-    client.services['Messenger']['ports']['MessengerSoap']['operations']['GetSmsStatus']['output']['GetSmsStatusResponse']['GetSmsStatusResult']['Statistics'] = {'statistics':statistics}
-
-    return client
+RECIPIENT = '375296543210'
 
 #
-# SendSms
+# Fixture
+#
+
+class HttpRequest:
+    def __init__(self, method):
+        self.method = method
+
+    def make(self, url, params):
+        req = None
+        if self.method == 'GET':
+            req = requests.get(url, params = params)
+            print("{0}".format(req.request.url))
+        elif self.method == 'POST':
+            req = requests.post(url, data = params)
+            print("{0} data: {1}".format(req.request.url, req.request.body))
+        return xmltodict.parse(req.text)
+
+@pytest.fixture(scope="function", params=["GET", "POST"])
+def request(request):
+    return HttpRequest(request.param)
+
+#
+# Utils
+#
+
+def send_sms(request, customerID, userName, userPassword, originator, smsText, recipientPhone, messageType, defDate, blink, flash, Private):
+    url = HOST + '/HTTP_SendSms'
+    params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword, \
+    'originator': originator, 'smsText': smsText, 'recipientPhone': recipientPhone, 'messageType': messageType, \
+    'defDate': defDate, 'blink': str(blink), 'flash': str(flash), 'Private': str(Private)}
+    return request.make(url, params)
+
+def get_sms_status(request, customerID, userName, userPassword, transactionID, detailed):
+    url = HOST + '/HTTP_GetSmsStatus'
+    params = {'customerID': str(customerID), 'userName': userName, 'userPassword': userPassword, \
+    'transactionID': transactionID, 'detailed': str(detailed)}
+    return request.make(url, params)
+
+
+#
+# Check encodings
 #
 
 # messageType=Latin|ArabicWithArabicNumbers|ArabicWithLatinNumbers
 
-def check_message_parts_count(client, message, encoding, count):
-    res = client.SendSms(user=USER, originator=ORIGINATOR, smsText=message, recipientPhone=RECIPIENT, messageType=encoding, defDate='', blink=False, flash=False, Private=False)
-    assert res['SendSmsResult']['Result'] == 'OK'
-    assert res['SendSmsResult']['TransactionID'] != None
-    tid = res['SendSmsResult']['TransactionID']
+def check_message_parts_count(request, message, encoding, count):
+    res = send_sms(request, CUSTOMER_ID, USER_ID, PASSWORD, ORIGINATOR, message, RECIPIENT, encoding, '', False, False, False)
+    assert res['SendResult']['Result'] == 'OK'
+    assert res['SendResult']['TransactionID'] != None
+    tid = res['SendResult']['TransactionID']
 
-    res = client.GetSmsStatus(user=USER, transactionID=tid, detailed=False)
-    assert res['GetSmsStatusResult']['Result'] == 'OK'
-    print res
-    assert sum(res['GetSmsStatusResult']['Statistics']['statistics'].values()) == count
+    res = get_sms_status(request, CUSTOMER_ID, USER_ID, PASSWORD, tid, False)
+    assert res['SmsStatus']['Result'] == 'OK'
+    stats = res['SmsStatus']['Statistics']['statistics']
+    # w/o namespace value, which is first
+    stats2 = stats.values()[1:]
+    assert sum(map(lambda d: int(d['#text']), stats2)) == count
 
-def test_check_encodings(client):
+def test_check_encodings(request):
     latin1_160 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJ'
     latin1_161 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJK'
     latin1_306 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345'
     latin1_307 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456'
     latin1_459 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxy'
     latin1_460 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'
+    utf8_70 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123'
+    utf8_71 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ01234'
+    utf8_134 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧ'
+    utf8_135 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШ'
+    utf8_201 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНО'
+    utf8_202 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОП'
     checks = [
         (latin1_160, 'Latin', 1),
         (latin1_161, 'Latin', 2),
         (latin1_306, 'Latin', 2),
         (latin1_307, 'Latin', 3),
         (latin1_459, 'Latin', 3),
-        (latin1_460, 'Latin', 4)
-        ## utf-8 encoding tests are coming
+        (latin1_460, 'Latin', 4),
+        (utf8_70,    'Latin', 1),
+        (utf8_71,    'Latin', 2),
+        (utf8_134,   'Latin', 2),
+        (utf8_135,   'Latin', 3),
+        (utf8_201,   'Latin', 3),
+        (utf8_202,   'Latin', 4)
     ]
 
     for (message, encoding, count) in checks:
-        check_message_parts_count(client, message, encoding, count)
+        check_message_parts_count(request, message, encoding, count)
